61cc840391f3b2d69f3bccb1590d5b1b36173b90
revnil <{ID}+{username}@users.noreply.github.com>
2018-12-15 11:35:42 -0800

    first commit

---
+++ change_wallpaper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while test $# -gt 0; do
+	case "$1" in
+		-q|--search)
+			shift
+			if test $# -gt 0; then
+				export SEARCH=$1
+			else
+				echo "no search string specified"
+				exit 1
+			fi
+			shift
+			;;
+		*)
+			break
+			;;
+	esac
+done
+
+wget https://source.unsplash.com/1920x1080/?"$SEARCH" > /dev/null 2>&1
+#wget https://unsplash.it/1920/1080/?random > /dev/null 2>&1
+mv ./index.html\?"$SEARCH" randompaper.jpg
+feh --bg-scale randompaper.jpg
+
