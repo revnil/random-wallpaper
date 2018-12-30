acf63fd21c3677d51e324705b58a65f7574e49ac
revnil <revnil@users.noreply.github.com>
2018-12-30 17:05:43 -0600

    Adding argument parser.

--- wpchange.py
+++ wpchange.py
@@ -4,11 +4,16 @@ import requests
 import subprocess
 import argparse
 
-parser = argparse.ArgumentParser(description='Changes the wallpaper.')
-parser.add_argument('--search', help='search for random wallpaper based on your input.')
-args = parser.parse_args()
 
 def change_wallpaper(
+
+parser = argparse.ArgumentParser(description="Changes the wallpaper.")
+parser.add_argument("--search", help="search for random wallpaper based on your input.")
+args = parser.parse_args()
+
+if args.search:
+
+#search = sys.argv[1]
 search = sys.argv[1]
 url = "https://source.unsplash.com/1920x1080/"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
