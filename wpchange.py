35b7dafe87e03666f3fa36f438a49461527b5679
revnil <revnil@users.noreply.github.com>
2018-12-30 17:26:11 -0600

    Adding argument parser.

--- wpchange.py
+++ wpchange.py
@@ -5,25 +5,24 @@ import subprocess
 import argparse
 
 
-def change_wallpaper(
+def change_wallpaper():
 
-parser = argparse.ArgumentParser(description="Changes the wallpaper.")
-parser.add_argument("--search", help="search for random wallpaper based on your input.")
-args = parser.parse_args()
+  parser = argparse.ArgumentParser(description="Changes the wallpaper.")
+  parser.add_argument("-s", "--search", help="search for random wallpaper based on your input.")
+  args = parser.parse_args()
 
-if args.search:
+  if args.search:
 
-#search = sys.argv[1]
-search = sys.argv[1]
-url = "https://source.unsplash.com/1920x1080/"
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+  #search = sys.argv[1]
+  search = sys.argv[1]
+  url = "https://source.unsplash.com/1920x1080/"
+  headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-r = requests.get(url,headers=headers,allow_redirects=True,params=search)
-open('default.jpg','wb').write(r.content)
+  r = requests.get(url,headers=headers,allow_redirects=True,params=search)
+  open('default.jpg','wb').write(r.content)
 
-subprocess.Popen('/usr/bin/feh --bg-scale default.jpg', shell=True)
+  subprocess.Popen('/usr/bin/feh --bg-scale default.jpg', shell=True)
 
-)
 
 if __name__ == '__main__':
     change_wallpaper()
