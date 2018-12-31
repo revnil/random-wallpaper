f9b886b83af211d7a51aa59e04f33b0308648092
revnil <revnil@users.noreply.github.com>
2018-12-30 18:32:02 -0600

    Removed commented code no longer needed.

--- wpchange.py
+++ wpchange.py
@@ -19,9 +19,10 @@ def change_wallpaper():
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
     r = requests.get(url,headers=headers,allow_redirects=True,params=search)
-    open('default.jpg','wb').write(r.content)
+    search.strip()
+    open('%search.jpg','wb').write(r.content)
 
-    subprocess.Popen('/usr/bin/feh --bg-scale default.jpg', shell=True)
+    subprocess.Popen('/usr/bin/feh --bg-scale %search.jpg', shell=True)
   else:
     parser.print_help()
     exit(1)
