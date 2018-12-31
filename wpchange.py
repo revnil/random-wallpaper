248cefd0d889d3ecd67c87182649d9797b5fbb39
revnil <{ID}+{username}@users.noreply.github.com>
2018-12-30 20:04:51 -0600

    Adding timestamp to wallpaper file and corrected spacing.

--- wpchange.py
+++ wpchange.py
@@ -16,16 +16,15 @@ def change_wallpaper():
 
     os.chdir(os.path.dirname(__file__))
     if args.search:
-        search = sys.argv[1]
+        search = sys.argv[2]
         url = "https://source.unsplash.com/1920x1080/"
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        r = requests.get(url, headers=headers, allow_redirects=True, params=search)
 
-        r = requests.get(url,headers=headers,allow_redirects=True,params=search)
-        search.replace(" ", "_")
-        wallpaper = search + timestr
-        open("%wallpaper.jpg",'wb').write(r.content)
+        filename = search.replace(' ', '-').lower() + '-' + timestr + '.jpg'
+        open(filename, 'wb').write(r.content)
 
-        subprocess.Popen("/usr/bin/feh --bg-scale %wallpaper.jpg", shell=True)
+        subprocess.Popen('/usr/bin/feh --bg-scale ' + filename, shell=True)
     else:
         parser.print_help()
         exit(1)
